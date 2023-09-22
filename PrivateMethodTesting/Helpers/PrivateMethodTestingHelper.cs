22b12dd1f196b18a5afcd5d781c8864637ca05c4
Dawid Sokol <dawid.sokol@ceneo.pl>
2023-09-22 13:55:38 +0200

    Add project files.

---
+++ PrivateMethodTesting/Helpers/PrivateMethodTestingHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+
+namespace PrivateMethodTesting.Helpers
+{
+    public static class PrivateMethodTestingHelper
+    {
+        internal static TReturn InvokePrivateMethod<TReturn>(
+            this object instance,
+            string methodName,
+            params object[] parameters)
+        {
+            Type type = instance.GetType();
+            BindingFlags bindingAttr = BindingFlags.NonPublic | BindingFlags.Instance;
+            MethodInfo method = type.GetMethod(methodName, bindingAttr);
+
+            return (TReturn)method.Invoke(instance, parameters);
+        }
+    }
+}
